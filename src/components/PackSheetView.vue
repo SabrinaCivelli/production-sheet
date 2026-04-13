4407bf8b595c6182b2903d58801af889ed5ce6ec
Sabrina Civelli Throp Kusel <sabrinacivelli@Sabrinas-Deliverect.local>
2026-04-13 12:35:34 -0400

    Changed button back to scope to back

--- src/components/PackSheetView.vue
+++ src/components/PackSheetView.vue
@@ -7,7 +7,7 @@
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M10 12.5L5 8L10 3.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
-        Back to Scope
+        Back
       </button>
       <button class="btn-dark" @click="printSheet">
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
